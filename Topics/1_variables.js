6b1cd7c3e66814e2dadd9fd4d5e791e811628cc5
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-10-20 23:47:37 +0530

    var-let-const

---
+++ Topics/1_variables.js
@@ -0,0 +1,9 @@
+console.log("hello world")
+let a = 55;
+console.log(a)
+a = "harry";
+console.log(a);
+a = "chetan";
+console.log(a);
+// let 8b = "harry" variable name start from numbers and var let const is not allowed
+// we can start from string doller sign 
---
+++ Topics/2_let-const-var.js
@@ -0,0 +1,15 @@
+// In let we can update (a) but cant redeclare it 
+let a = 15;
+console.log(a);
+a = "harry";
+console.log(a);
+
+//In var both we can redeclare and update it
+var b = 10;
+console.log(b);
+var b = 20;
+console.log(b);
+
+//In const once we Declare after that we cant not update or redclare it
+const c = 10;
+console.log(c);

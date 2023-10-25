b41797fb6265165e7849b2071258825fd5c72fa1
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-10-26 00:20:10 +0530

    practice-set-1

---
+++ Topics/practice-set1.js
@@ -0,0 +1,27 @@
+// add string and number in a console
+let a = "chetan";
+let b = 50;
+console.log(a+b);
+
+// check the above question type
+console.log(typeof(a+b))
+
+//in const object we can add a object
+const c = {
+    "name" : "adesh",
+    "roll" : 45,
+    "college" : "mit"
+}
+c["address"] = "bajajnagar aurangabad";
+
+console.log(c);
+
+//five word dictionary
+const dict = {
+    name1 : "adesh",
+    name2 : "chetan",
+    name3 : "harish",
+    name4 : "pratham"
+}
+console.log(dict.name2);
+// we can access object item using . (dot)

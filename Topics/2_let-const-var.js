2a8bbe2381bda4abba4f333b19c152554d4a7479
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-10-25 23:56:24 +0530

    primitive-datatypes

--- Topics/2_let-const-var.js
+++ Topics/2_let-const-var.js
@@ -3,12 +3,21 @@ let a = 15;
 console.log(a);
 a = "harry";
 console.log(a);
+//it is also a block scope
+let d = 10;
+{
+    let d = 20;
+    console.log(d);
+}
+console.log(d);
+
 
 //In var both we can redeclare and update it
 var b = 10;
 console.log(b);
 var b = 20;
 console.log(b);
+
 
 //In const once we Declare after that we cant not update or redclare it
 const c = 10;
---
+++ Topics/3_primitive-datatype.js
@@ -0,0 +1,27 @@
+//N=null, N=number, S=string ,S=symbole ,B=boolean ,B=bigint,U=undefine
+
+let a = null;
+let b = 345;
+let c = "chetan";
+let d = Symbol("i am nice symbol");
+let e = true;
+let f = BigInt("14263");
+let g = undefined;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+//how to check the type of datatype
+console.log(typeof f);
+
+//objects in javascript
+const item = {
+    "name" : "chetan",
+    "rollnumber" : 1095,
+    "college" : "cu"
+}
+console.log(item["name"])

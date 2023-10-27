a07d26c267b4d299cb0a69dd410adf03af6ebfa2
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-10-27 09:58:19 +0530

    practice-set-2

---
+++ Topics/practice-set2.js
@@ -0,0 +1,37 @@
+// using logical operator find whether the the of peron lies between 10 to 20
+// let a = 21;
+// if (a>10 && a<20){
+//     console.log("your age is between 10 to 20")
+// }else if(a>20){
+//     console.log("your age is above 20")
+// }else{
+//     console.log("your age is below 10")
+// }
+
+// switch case question 
+// let a = 20;
+// let b = 10;
+// let op = "-";
+// switch(op){
+//     case '+' : console.log(a+b);
+//     break
+//     case '-' : console.log(a-b);
+//     break
+//     case '/' : console.log(a/b);
+//     break
+//     case '*' : console.log(a*b);
+//     break
+// }
+
+// let a = 6;
+// if (a%2 == 0 && a%3 == 0){
+//     console.log("number is divided by both")
+// }else if (a%3 == 0){
+//     console.log("number is divided by 3")
+// }else if (a%2 == 0){
+//     console.log("number is divided by 2")
+// }
+
+// ternary operator
+// let a = 2;
+// console.log(a>18 ? 'you can drive' : 'you can not drive')

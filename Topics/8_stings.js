666f03692f43d2bb5180590db963fd676ea89ee0
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-02 00:05:06 +0530

    string

---
+++ Topics/8_stings.js
@@ -0,0 +1,18 @@
+let name = "chetan"
+console.log(name.length) // length
+console.log(name[2]) //printing char obj
+
+//tamplate literals
+let boy1 = "harry"
+let boy2 = "chetan"
+let print = `${boy1} is a friend of ${boy2}`
+console.log(print)
+
+//escape sequence
+let fruit = 'Ban\'ana';
+console.log(fruit)
+// \n for newline
+// \t for tab
+// \r carriage return
+
+

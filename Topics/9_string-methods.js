372fe047c78b48b59df65533b86437b995b19cd1
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-05 23:20:44 +0530

    string-methods

---
+++ Topics/9_string-methods.js
@@ -0,0 +1,35 @@
+// string methods
+
+//how to find the length of string
+let name1 = "chetan"
+console.log(name1.length) //6
+
+//how to convert a string in a uppecase
+let name2 = "chetan"
+console.log(name2.toUpperCase()) //CHETAN
+
+//how to convert to lowercase
+let name3 = "CHETAN"
+console.log(name3.toLowerCase()) //chetan
+
+//how to slice a string
+let name4 = "chetan"
+console.log(name4.slice(2,4)) //et
+console.log(name4.slice(2)) //etan
+
+//how to replace a string char in javascript
+let name5 = "chetan salunke"
+console.log(name5.replace("chetan","adesh"))
+
+//how to concat a string in javascript
+let name6 = "chetan"
+let name7 = "adesh"
+console.log(name7.concat(" is a brother of ",name6 )) //adesh is a brother of chetan
+
+//how to trim a string
+let name8 = "      chetan      "
+console.log(name8.trim())  //it will remove all the spaces
+
+
+
+

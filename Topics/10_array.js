d74e5c3dcdd5c502337ffa2d601d6aa19648b0e9
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-06 23:24:35 +0530

    arrays

---
+++ Topics/10_array.js
@@ -0,0 +1,18 @@
+let name = ["chetan","harish","adesh","pratham"];
+let marks = [25,26,24,25,false,null]; // we can also add false null 
+console.log(marks.length) // to find the length of array
+console.log(marks[2]) // to find the indexing array
+
+//changing the value
+let marks2 = [12,14,15,25]
+console.log(marks2[2]=8); // 2 is indexing and it will change by 8
+console.log(marks2)
+
+//we can also add new array
+let marks3 = [1,2,3,4,5]
+console.log(marks3[5]=6)
+console.log(marks3)
+
+//type of operator
+let name2 = ["chetan","adesh","harish"]
+console.log(typeof name2)

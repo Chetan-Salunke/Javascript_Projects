e83be1a1bae337ee67f9d5931760383d9cd0a61b
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-21 02:49:46 +0530

    javascript_objects

---
+++ Topics/11_objects.js
@@ -0,0 +1,16 @@
+let student = {
+    name : "chetan",
+    roll : 1095,
+    age : 21,
+    cgpa : 7.2,
+    isPass : true
+};
+
+console.log(student); // normal print
+
+console.log(student.cgpa) // specific key
+
+student.age = student.age+1;
+student.name = student.name+"salunke";
+console.log(student.age)
+console.log(student.name)

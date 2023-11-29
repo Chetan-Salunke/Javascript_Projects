66bb7262bed5e509c0352cc6d6a0ad3b92489065
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-29 23:29:25 +0530

    foreach-loop

---
+++ Topics/13_foreach-loop.js
@@ -0,0 +1,9 @@
+
+// when we want to do some work with each element of array we used foreach loop
+// arr.from = used to creat an array from any other objects
+
+let arr = [1,2,3,4,5];
+
+arr.forEach((value) => {
+    console.log(value*2)
+})

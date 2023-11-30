74789bc260875fd2a0a10b4a9def56596a999631
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-11-30 22:41:48 +0530

    console-objects

---
+++ Topics/15_console-object.js
@@ -0,0 +1,11 @@
+// console objects
+
+console.log("hello world") //print the message
+console.assert(4<5) // check the condition
+console.clear() // clear the console 
+console.error("hello world2") // give the error for our understanding
+let a = {a:3, b:4, c:5, d:6}
+console.table(a);
+console.warn("no drink and drive")
+console.info("important message")
+console.time() // to check the time taken to run

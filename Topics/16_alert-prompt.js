1e3acc3c355a03c95cd1eff232b2277dea986cd2
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-01 23:56:51 +0530

    alert-confirm-prompt

---
+++ Topics/16_alert-prompt.js
@@ -0,0 +1,6 @@
+alert("hey hello world") // to give a small window popup meaaage
+
+let a = prompt("enter the value")
+document.write(a) // take a user input in form of string
+
+// confirm give you a true or false value

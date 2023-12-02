0e4d089751833721f2891a37a2fd146821bd03e2
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-02 23:55:42 +0530

    DOM-BOM

--- Topics/16_alert-prompt.js
+++ Topics/16_alert-prompt.js
@@ -1,6 +1,8 @@
-alert("hey hello world") // to give a small window popup meaaage
+// alert("hey hello world") // to give a small window popup meaaage
 
-let a = prompt("enter the value")
-document.write(a) // take a user input in form of string
+// let a = prompt("enter the value")
+// document.write(a) // take a user input in form of string
 
 // confirm give you a true or false value
+
+document.body.style.backgroundColor = "green"
---
+++ Topics/17_dom-bom.js
@@ -0,0 +1,6 @@
+//DOM - dom is a document object model 
+//we can access via document.body.style.background="red";
+
+
+//BOM - bom is a browser object model 
+//alert, confirm, prompt, are the part of bom 

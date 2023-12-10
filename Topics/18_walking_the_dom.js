4d73fc9474734b499fed2d021bf03a96e051e1ac
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-10 14:17:32 +0530

    walking-the-dom

---
+++ Topics/18_walking_the_dom.js
@@ -0,0 +1,27 @@
+// document object model
+// 1) text node 2) element node 3)comment node
+
+
+//parent node
+let b = document.getElementById("container3")
+// console.log("hello")
+// console.log(b.parentNode)
+
+
+//firstchild & last child
+let a = document.getElementById("container")
+// let firstChild = a.firstChild;
+// console.log(a.firstChild)
+
+
+//same for table props 
+// table.rows
+//table.caption
+//table.thead
+//table.tfoot
+//table.rows
+//table.tbodies
+
+
+
+

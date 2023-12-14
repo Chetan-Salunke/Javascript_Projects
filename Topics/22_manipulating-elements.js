5ded90f54f726caf3aafd53edafb32d1bda5f6ec
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-15 00:40:54 +0530

    settimeout-setintervial

--- Topics/22_manipulating-elements.js
+++ Topics/22_manipulating-elements.js
@@ -8,3 +8,16 @@
 // for (let cssClass of div.classList){
 //     console.log(div);
 // }
+
+
+//HTML -  <div id="first" className="background-red textblack">helo world</div>
+
+//CSS - .background-red{ background-color = "red"}
+     // .background -yellow{ background-color ="yellow"}
+     // .textblack{color = "black"}
+
+//JS - first.className = "background-yellow textblack"
+//first.classlist.remove = "background-yellow"
+//first.classlist.add = "background-red"
+//first.classlist.toggle = is used for add a class
+//first.classlist.contain('class') - return true or false
---
+++ Topics/23_settimeout-setintervial.js
@@ -0,0 +1,17 @@
+
+// set time out
+// console.log("hello")
+// let a = setTimeout(() => {
+//     console.log("done")
+// }, 5000);
+// console.log("world")
+
+//clear time out  - to stop the set tie out
+
+//set intervial
+// console.log("hello")
+// let a = setInterval(() => {
+//    console.log("world") 
+// }, 2000);
+
+//settimeout(function(), delay, argument1, argument2)

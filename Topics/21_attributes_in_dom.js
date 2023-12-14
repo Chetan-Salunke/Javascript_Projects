e2427e72352f1572d19007468ce008ea62013993
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-14 23:17:29 +0530

    attributes-in-dom

---
+++ Topics/21_attributes_in_dom.js
@@ -0,0 +1,11 @@
+// get attribute is used to get value
+// let a = first.getAttribute('class')
+// console.log(a)
+
+
+// set attribute is used to set attribute
+// let b = document.getElementById("first").setAttribute("className", "my-class")
+// console.log(b);
+
+
+// has attribute is used to check the attribute is present or not its always give true or false

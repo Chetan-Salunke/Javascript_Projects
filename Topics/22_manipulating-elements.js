8d6afca11071d1dc0689f580611d1db2898c3143
Chetan-Salunke <chetanbusalunke@gmail.com>
2023-12-14 23:59:48 +0530

    manipulating-event

---
+++ Topics/22_manipulating-elements.js
@@ -0,0 +1,10 @@
+// classname manipulating its return a class 
+// let a = document.querySelector('#menu');
+// console.log(a.className)
+
+
+//classlist is return all space name in different line
+// let div = document.querySelector('#menu');
+// for (let cssClass of div.classList){
+//     console.log(div);
+// }
